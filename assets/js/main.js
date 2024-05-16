250ee935c38a010723cfb2d52c921261cefb67a6
jesslyngowin <118154281+jesslyngowin@users.noreply.github.com>
2024-05-16 11:11:20 +0700

    remove yg gk perlu

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,25 +206,6 @@ function showTab(n) {
   fixStepIndicator(n)
 }
 
-// function nextPrev(n) {
-//   // This function will figure out which tab to display
-//   var x = document.getElementsByClassName("step");
-//   // Exit the function if any field in the current tab is invalid:
-//   if (n == 1 && !validateForm()) return false;
-//   // Hide the current tab:
-//   x[currentTab].style.display = "none";
-//   // Increase or decrease the current tab by 1:
-//   currentTab = currentTab + n;
-//   // if you have reached the end of the form...
-//   if (currentTab >= x.length) {
-//     // ... the form gets submitted:
-//     document.getElementById("signUpForm").submit();
-//     return false;
-//   }
-//   // Otherwise, display the correct tab:
-//   showTab(currentTab);
-// }
-
 function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("step");
